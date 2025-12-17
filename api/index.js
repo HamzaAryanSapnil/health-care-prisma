768d30aaba518c7b8d0158b3b55d268b4ede0508
Hamza Aryan Sapnil <hamzaswapnil@gmail.com>
2025-12-18 01:01:29 +0600

    let's see if it works

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function entry point
-// Import the Express app (not the server bootstrap)
-const app = require('../dist/app.js').default;
-
-// Export as Vercel serverless function handler
-module.exports = app;
-
