e1286acc9d6c6fdc24ce2ee365c01e263303d5f7
Hamza Aryan Sapnil <hamzaswapnil@gmail.com>
2025-12-18 09:34:27 +0600

    Refactor render-build.sh to replace deprecated database commands with prisma generate and migrate deploy

--- render-build.sh
+++ render-build.sh
@@ -2,10 +2,5 @@ set -o errexit
 
 bun install
 bun run build
-bun run build:all
-bun run db:generate
-bun run db:migrate
-bun run db:push
-bun run db:pull
-bun run db:seed
-bun run dev
+bunx --bun prisma generate 
+bunx --bun prisma migrate deploy
